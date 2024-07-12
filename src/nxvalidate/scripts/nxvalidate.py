37cd5978a9be97b50225433b0b89dcff670ad19e
Ray Osborn <rayosborn@mac.com>
2024-07-12 10:06:41 -0500

    Create an installable package

---
+++ src/nxvalidate/scripts/nxvalidate.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+
+"""
+NXVALIDATE PY
+"""
+
+import logging
+import os
+import sys
+
+import xml.etree.ElementTree as ET
+
+from nxvalidate.validate import GroupValidator, FieldValidator, validate
+
+
+def main():
+    """ Outline of program """
+    logger = logging.getLogger("nxvalidate")
+    setup_logger(logger)
+    logger.info("NXVALIDATE")
+    settings = load_defaults()
+    args = parse_args()
+    apply_args(logger, args, settings)
+    try:
+        do_command(logger, args, settings)
+    except UserError as e:
+        print("nxvalidate: user error: " + " ".join(e.args))
+        exit(1)
+
+
+class UserError(Exception):
+    """ Generic user error for the nxvalidate program. """
+    pass
+
+
+def setup_logger(logger):
+    """ Set up the logger for development use """
+    logger.setLevel(logging.DEBUG)
+    h = logging.StreamHandler(stream=sys.stdout)
+    fmtr = logging.Formatter(
+            "%(asctime)s %(name)s %(levelname)-5s %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S")
+    h.setFormatter(fmtr)
+    logger.addHandler(h)
+    return logger
+
+
+def load_defaults():
+    """ Look up default settings from environment """
+    settings = {}
+    settings["definitions"] = env_default("NEXUS_DEFINITIONS")
+    return settings
+
+
+def env_default(name, default=None):
+    """ Helper function to deal with environment variables """
+    result = None
+    value = os.getenv(name)
+    if value is not None and len(value) > 0:
+        result = value
+    return result
+
+
+def parse_args():
+    """
+    Use argparse to handle command line
+    Probably should match and extend cnxvalidate for sanity
+    """
+    import argparse
+    parser = argparse.ArgumentParser(
+        prog="nxvalidate",
+        description="Validates NXS files.")
+    parser.add_argument("-l",
+                        action="store",
+                        help="application definition directory")
+    parser.add_argument("command",
+                        help="one of: 'report' or ...")
+    parser.add_argument("extras", nargs="*",
+                        help="Additional arguments")
+    args = parser.parse_args()
+    return args
+
+
+def apply_args(logger, args, settings):
+    """
+    Apply any arguments
+    overwriting any defaults from the environment
+    """
+    if "l" in args:
+        settings["definitions"] = args.l
+
+
+def do_command(logger, args, settings):
+    """ Branch into the user command """
+    if args.command == "report":
+        do_report(logger, args, settings)
+    # TODO: Add other commands here
+
+
+def do_report(logger, args, settings):
+    """ Execute the report """
+    logger.info("do_report...")
+    if len(args.extras) != 1:
+        raise UserError("report requires 1 token!")
+    token = args.extras[0]
+    logger.debug("requested: '%s'" % token)
+    base_classes = get_base_classes(settings)
+    xml_file = base_classes + "/" + token + ".nxdl.xml"
+    logger.debug("open: " + xml_file)
+    try:
+        with open(xml_file) as fp:
+            report_items(fp, token)
+    except FileNotFoundError as e:
+        raise UserError("could not open XML definition: " +
+                        e.filename)
+
+
+def report_items(fp, token):
+    """ Actually scan the XML and print the report """
+    # Parse the XML:
+    tree = ET.parse(fp)
+    root = tree.getroot()
+    # Lookup some header metadata
+    if "extends" in root.attrib:
+        extends = root.attrib["extends"]
+        print("%s extends %s" % (token, extends))
+    # These will be our "rules":
+    groups = []
+    attributes = []
+    fields = []
+    # Scan through the XML tree for entries about our rules:
+    for child in root:
+        tag = get_tail(child.tag)
+        if tag == "group":
+            groups.append(child.attrib["type"])
+        elif tag == "attribute":
+            attributes.append(child.attrib["name"])
+        elif tag == "field":
+            fields.append(child.attrib["name"])
+    # Report groups:
+    print("%s may contain these %i groups:" %
+          (token, len(groups)))
+    for group in groups:
+        print("\t " + group)
+    # Report attributes:
+    print("%s may contain these %i attributes:" %
+          (token, len(attributes)))
+    for attribute in attributes:
+        print("\t " + attribute)
+    # Report fields:
+    print("%s may contain these %i fields:" %
+          (token, len(fields)))
+    for field in fields:
+        print("\t " + field)
+
+
+def get_base_classes(settings):
+    """ Find the directory named base_classes """
+    if settings["definitions"] is None:
+        raise UserError("application definition directory " +
+                        "not specified!")
+    if not os.path.exists(settings["definitions"]):
+        raise UserError("given definitions do not exist!")
+    p = settings["definitions"] + "/../base_classes"
+    base_classes = os.path.abspath(p)
+    if not os.path.exists(base_classes):
+        raise UserError("base_classes do not exist!")
+    return base_classes
+
+
+def get_tail(s):
+    """ Remove the namespace portion from the XML tag """
+    tokens = s.split("}")
+    return tokens[1]
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/nxvalidate/validate.py
@@ -0,0 +1,94 @@
+import xml
+import xml.etree.ElementTree as ET
+from importlib.resources import files
+
+import numpy as np
+from nexusformat.nexus import *
+
+filename = files('nxvalidate.examples').joinpath('chopper.nxs')
+valid_groups = {}
+
+def get_valid_entries(base_class, tag):
+    file_path = files('nxvalidate.definitions.base_classes').joinpath(
+        f'{base_class}.nxdl.xml')
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+    valid_list = []
+    
+    if tag.lower() == 'field':
+        valid_list = []
+        for field in root.findall('.//{http://definition.nexusformat.org/nxdl/3.1}field'):
+            name = field.get('name')
+            if name:
+                valid_list.append(name)       
+                     
+    elif tag.lower() == 'group':
+        valid_list = []
+        for group in root.findall('.//{http://definition.nexusformat.org/nxdl/3.1}group'):
+            type = group.get('type')
+            if type:
+                valid_list.append(type)
+    else:
+        print('Invalid function argument: must be field or group')
+    
+    return valid_list 
+
+class GroupValidator():
+
+    def __init__(self, group):
+        self.group = group
+        self.nxclass = self.group.nxclass
+        self.valid_fields = self.get_valid_fields()
+        self.valid_groups = self.get_valid_groups()
+        if self.group.nxgroup:
+            self.validator = GroupValidator(self.group.nxgroup)
+        else:
+            self.validator = None
+
+    def get_valid_groups(self):
+        if self.nxclass in valid_groups:
+            return valid_groups[self.nxclass]
+        try:
+            valid_groups[self.nxclass] = get_valid_entries(self.nxclass, 'group')
+            return valid_groups[self.nxclass]
+        except Exception as error:
+            return []
+
+    def get_valid_fields(self):
+        return get_valid_entries(self.nxclass, 'field')
+
+    def validate(self):
+        if self.validator is not None:
+            if self.nxclass in self.validator.valid_groups:
+                print(f'{self.group.nxname}:{self.group.nxclass}')
+            else:
+                print(f'{self.group.nxname} unspecified')
+
+
+class FieldValidator():
+
+    def __init__(self, field):
+        self.field = field
+        self.validator = GroupValidator(self.field.nxgroup)
+
+    def validate(self):
+        if self.field.nxname in self.validator.valid_fields:
+            print(f'{self.field.nxname}: value={self.field.nxvalue}')
+        else:
+            print(f'{self.field.nxname} unspecified')
+
+
+def validate():
+
+    with nxopen(filename) as root:
+        for item in root.walk():
+            if isinstance(item, NXgroup):
+                validator = GroupValidator(item)
+            elif isinstance(item, NXfield):
+                validator = FieldValidator(item)
+            if validator is not None:
+                validator.validate()
+
+
+if __name__ == "__main__":
+    validate()
