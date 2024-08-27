d7641fec35b7aacd7d6c45ac9f0c868484fc0bae
Ray Osborn <rayosborn@mac.com>
2024-08-27 09:08:19 -0500

    Add variable-length string type to NX_CHAR validation

--- src/nxvalidate/utils.py
+++ src/nxvalidate/utils.py
@@ -31,29 +31,38 @@ def is_valid_iso8601(date_string):
 def is_valid_int(dtype):
     return np.issubdtype(dtype, np.integer) 
 
+
 def is_valid_float(dtype):
     return np.issubdtype(dtype, np.floating)
+
 
 def is_valid_bool(dtype):
     return np.issubdtype(dtype, np.bool_) 
 
+
 def is_valid_char(dtype):
-    return np.issubdtype(dtype, np.str_) or  np.issubdtype(dtype, np.bytes_)
+    return (np.issubdtype(dtype, np.str_) or  np.issubdtype(dtype, np.bytes_)
+            or dtype == string_dtype)
+
 
 def is_valid_char_or_number(dtype):
-    return np.issubdtype(dtype, np.number) or np.issubdtype(dtype, np.str_) 
+    return is_valid_char(dtype) or is_valid_number(dtype)
+
 
 def is_valid_complex(dtype):
     return np.issubdtype(dtype, np.complex) 
 
+
 def is_valid_number(dtype):
     return np.issubdtype(dtype, np.number) 
+
 
 def is_valid_posint(dtype):
     if np.issubdtype(dtype, np.integer):
          info = np.iinfo(dtype)
          return info.max > 0
     return False 
+
 
 def is_valid_uint(dtype):
     return np.issubdtype(dtype, np.unsignedinteger) 
